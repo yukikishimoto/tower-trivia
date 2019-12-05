c0e5fbef6904f9b5f5ad970fc1209f3d28257c7a
Yuki Kishimoto <kishimoto.yuki@gmail.com>
2019-12-05 16:50:17 -0500

    Updated back-end endpoint for highscores route to write to data file

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-const game = require('./routes/game');
-const highscores = require('./routes/highscores');
+const fs = require('fs');
+const shortid = require('short-id');
+const gameroute = require('./routes/game');
+const highscoresroute = require('./routes/highscores');
+const highscores = require('./data/highscores.json');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/game', game);
-app.use('/highscores', highscores);
+app.use('/game', gameroute);
+app.use('/highscores', highscoresroute);
+
+// POST request for the /highscores route is placed in this file because the fs.writeFile() function could not recognize the correct file path from the routes folder:
+app.post('/highscores', (request, response) => {
+  const newHighScore = {
+    "id": shortid.generate(),
+    "name": request.body.name,
+    "score": request.body.score
+  };
+  highscores.push(newHighScore);
+  fs.writeFile('./data/highscores.json', JSON.stringify(highscores), (err) => {
+    if (err) throw err;
+    console.log("write file worked");
+  });
+  response.send(highscores);
+})
 
 app.listen(8080, () => {
-    console.log("Server started on http://localhost:8080");
+  console.log("Server started on http://localhost:8080");
 })
--- server/routes/highscores.js
+++ server/routes/highscores.js
@@ -1,20 +1,9 @@
 const express = require('express');
 const highscores = require('../data/highscores.json');
-const shortid = require('short-id');
 
 const router = express.Router();
 
 router.get('/', (request, response) => {
-  response.send(highscores);
-})
-
-router.post('/', (request, response) => {
-  const newHighScore = {
-    "id": shortid.generate(),
-    "name": request.body.name,
-    "score": request.body.score
-  };
-  highscores.push(newHighScore);
   response.send(highscores);
 })
 
