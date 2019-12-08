08eda73589bc5707cd17eaf241b8b79a75130cbe
Yuki Kishimoto <kishimoto.yuki@gmail.com>
2019-12-08 18:22:35 -0500

    Moved code for Learn More feature to new LearnMore component

---
+++ client/src/components/LearnMore/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Modal from 'react-modal';
+import Slider from 'react-slick';
+import close from '../../assets/icons/close.svg';
+import './styles.scss';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+function LearnMore(props) {
+  const {showModal, questionsSeen, handleCloseModal} = props;
+
+  return (
+    <Modal isOpen={showModal}>
+      <img className="learn-more__modal-close" src={close} alt="" onClick={handleCloseModal} />
+      <Slider dots={true} cssEase="cubic-bezier(0.68, -0.55, 0.265, 1.55)" speed="400" >
+        {
+          questionsSeen[0] ? 
+            questionsSeen.map(answer => 
+              <div className="learn-more__tower-carousel-tab" key={answer.id}>
+                <img className="learn-more__tower-image" src={answer.image} alt="Famous tower" />
+                <h1 className="learn-more__tower-name">{answer.name}</h1>
+                <h2 className="learn-more__tower-location">{answer.correctAnswer}</h2>
+                <p>{answer.description}</p>
+              </div>
+            )
+          :
+          null
+        }
+      </Slider>
+    </Modal>
+  );
+}
+
+export default LearnMore;
--- client/src/pages/Game/index.js
+++ client/src/pages/Game/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Modal from 'react-modal';
-import Slider from 'react-slick';
 import axios from 'axios';
 import Tower from '../../components/Tower';
 import Trivia from '../../components/Trivia';
 import Result from '../../components/Result';
-import close from '../../assets/icons/close.svg';
+import LearnMore from '../../components/LearnMore';
 import './styles.scss';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 
 class Game extends React.Component {
   constructor(props) {
@@ -135,7 +132,7 @@ class Game extends React.Component {
 
   render() {
     const {history} = this.props;
-    const {trivia, currentQuestion, isQuestionAnswered, optionSelected, isAnswerCorrect, isGameOver, didPlayerWin, score, hook2Class, hook3Class, hook4Class, hook5Class} = this.state;
+    const {trivia, currentQuestion, isQuestionAnswered, optionSelected, isAnswerCorrect, isGameOver, didPlayerWin, score, showModal, hook2Class, hook3Class, hook4Class, hook5Class} = this.state;
 
     const questionsSeen = [];
     for (let i = 1; i <= this.state.currentQuestion; i++) {
@@ -146,24 +143,7 @@ class Game extends React.Component {
       <main className="game">
         {isGameOver ? <Result history={history} currentQuestion={currentQuestion} didPlayerWin={didPlayerWin} score={score} playAgainClickHandler={this.playAgainClickHandler} handleOpenModal={this.handleOpenModal} /> : <Tower score={score} hook2Class={hook2Class} hook3Class={hook3Class} hook4Class={hook4Class} hook5Class={hook5Class} />}
         <Trivia trivia={trivia} currentQuestion={currentQuestion} isQuestionAnswered={isQuestionAnswered} optionSelected={optionSelected} isAnswerCorrect={isAnswerCorrect} answerClickHandler={this.answerClickHandler} nextQuestionClickHandler={this.nextQuestionClickHandler} />
-        <Modal isOpen={this.state.showModal}>
-          <img className="game__modal-close" src={close} alt="" onClick={this.handleCloseModal} />
-          <Slider dots={true} cssEase="cubic-bezier(0.68, -0.55, 0.265, 1.55)" speed="400" >
-            {
-              questionsSeen[0] ? 
-                questionsSeen.map(answer => 
-                  <div className="game__tower-carousel-tab" key={answer.id}>
-                    <img className="game__tower-image" src={answer.image} alt="Famous tower" />
-                    <h1 className="game__tower-name">{answer.name}</h1>
-                    <h2 className="game__tower-location">{answer.correctAnswer}</h2>
-                    <p>{answer.description}</p>
-                  </div>
-                )
-              :
-              null
-            }
-          </Slider>
-        </Modal>
+        <LearnMore showModal={showModal} questionsSeen={questionsSeen} handleCloseModal={this.handleCloseModal}/>
       </main>
     )
   }
