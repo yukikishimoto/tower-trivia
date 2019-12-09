884936f46c8061a2c4bdc6638b257098f6e019a4
Yuki Kishimoto <kishimoto.yuki@gmail.com>
2019-12-09 17:18:11 -0500

    Updated props for LearnMore component to close the modal when the user clicks outside the modal

--- client/src/components/LearnMore/index.js
+++ client/src/components/LearnMore/index.js
@@ -10,7 +10,7 @@ function LearnMore(props) {
   const {showModal, questionsSeen, handleCloseModal} = props;
 
   return (
-    <Modal isOpen={showModal}>
+    <Modal isOpen={showModal} onRequestClose={handleCloseModal} shouldCloseOnOverlayClick={true}>
       <img className="learn-more__modal-close" src={close} alt="" onClick={handleCloseModal} />
       <Slider dots={true} cssEase="cubic-bezier(0.68, -0.55, 0.265, 1.55)" speed="400" >
         {
